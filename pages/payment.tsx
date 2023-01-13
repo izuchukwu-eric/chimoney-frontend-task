d86d9f6d95a79d89696e87bfb07bee72a95be39a
izuchukwu-eric <onukwubeizu@gmail.com>
2023-01-13 01:55:54 +0100

    new commit

--- pages/payment.tsx
+++ pages/payment.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import type { NextPage } from 'next'
 import Header from '../components/Header/Header'
-import Payment from '../components/Payment/payment'
+import Payment from '../components/Payment/Payment'
 
 const PaymentPage: NextPage = () => {
   return (
